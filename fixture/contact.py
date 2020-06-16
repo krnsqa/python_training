3f92b60c761d1101c8c811f563b37b6c7ecd94e4
Krn <katerina.nnnn@gmail.com>
2020-06-16 17:12:03 -0500

    Создан test_edit_group и test_edit_contact.

--- fixture/contact.py
+++ fixture/contact.py
@@ -36,3 +36,33 @@ class ContactHelper:
         dw.find_element_by_css_selector("input[value=\"Delete\"]").click()
         # confirm deletion
         dw.switch_to_alert().accept()
+
+
+    def clear_contact_form(self):
+        dw = self.app.dw
+        dw.find_element_by_name("firstname").clear()
+        dw.find_element_by_name("lastname").clear()
+        dw.find_element_by_name("address").clear()
+        dw.find_element_by_name("mobile").clear()
+        dw.find_element_by_name("email").clear()
+        dw.find_element_by_name("bday").send_keys("")
+        dw.find_element_by_name("bmonth").send_keys("")
+
+
+    def edit_first_contact(self, contact):
+        dw = self.app.dw
+        # select first contact
+        dw.find_element_by_css_selector("#maintable a[href^='edit']").click()
+        # clear edit contact form
+        self.clear_contact_form()
+        # fill out edit contact form
+        dw.find_element_by_name("firstname").send_keys(contact.firstname)
+        dw.find_element_by_name("lastname").send_keys(contact.lastname)
+        dw.find_element_by_name("address").send_keys(contact.address)
+        dw.find_element_by_name("mobile").send_keys(contact.mobile)
+        dw.find_element_by_name("email").send_keys(contact.email)
+        dw.find_element_by_name("bday").send_keys(contact.bday)
+        dw.find_element_by_name("bmonth").send_keys(contact.bmonth)
+        # submit edit contact form
+        dw.find_element_by_name("update").click()
+        self.return_to_homepage()
--- fixture/group.py
+++ fixture/group.py
@@ -40,3 +40,26 @@ class GroupHelper:
         self.return_to_groups_page()
 
 
+    def clear_group_form(self):
+        dw = self.app.dw
+        dw.find_element_by_name("group_name").clear()
+        dw.find_element_by_name("group_header").clear()
+        dw.find_element_by_name("group_footer").clear()
+
+
+    def edit_first_group(self, group):
+        dw = self.app.dw
+        self.open_groups_page()
+        # select first group
+        dw.find_element_by_name("selected[]").click()
+        # submit edition
+        dw.find_element_by_name("edit").click()
+        # clear edit group form
+        self.clear_group_form()
+        # fill out edit group form
+        dw.find_element_by_name("group_name").send_keys(group.name)
+        dw.find_element_by_name("group_header").send_keys(group.header)
+        dw.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit edit group form
+        dw.find_element_by_name("update").click()
+        self.return_to_groups_page()
