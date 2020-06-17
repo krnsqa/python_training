7be082db67d61809f877f2de054f939652571171
Krn <katerina.nnnn@gmail.com>
2020-06-17 16:37:43 -0500

    Создан метод fill_group_form и fill_contact_form.

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,11 +6,8 @@ class ContactHelper:
         self.app = app
 
 
-    def create(self, contact):
+    def fill_contact_form(self, contact):
         dw = self.app.dw
-        # init contact creation
-        dw.find_element_by_link_text("add new").click()
-        # fill out contact form
         dw.find_element_by_name("firstname").send_keys(contact.firstname)
         dw.find_element_by_name("lastname").send_keys(contact.lastname)
         dw.find_element_by_name("address").send_keys(contact.address)
@@ -18,6 +15,13 @@ class ContactHelper:
         dw.find_element_by_name("email").send_keys(contact.email)
         dw.find_element_by_name("bday").send_keys(contact.bday)
         dw.find_element_by_name("bmonth").send_keys(contact.bmonth)
+
+
+    def create(self, contact):
+        dw = self.app.dw
+        # init contact creation
+        dw.find_element_by_link_text("add new").click()
+        self.fill_contact_form(contact)
         # submit contact form
         dw.find_element_by_name("submit").click()
         self.return_to_homepage()
@@ -55,14 +59,7 @@ class ContactHelper:
         dw.find_element_by_css_selector("#maintable a[href^='edit']").click()
         # clear edit contact form
         self.clear_contact_form()
-        # fill out edit contact form
-        dw.find_element_by_name("firstname").send_keys(contact.firstname)
-        dw.find_element_by_name("lastname").send_keys(contact.lastname)
-        dw.find_element_by_name("address").send_keys(contact.address)
-        dw.find_element_by_name("mobile").send_keys(contact.mobile)
-        dw.find_element_by_name("email").send_keys(contact.email)
-        dw.find_element_by_name("bday").send_keys(contact.bday)
-        dw.find_element_by_name("bmonth").send_keys(contact.bmonth)
+        self.fill_contact_form(contact)
         # submit edit contact form
         dw.find_element_by_name("update").click()
         self.return_to_homepage()
--- fixture/group.py
+++ fixture/group.py
@@ -11,15 +11,19 @@ class GroupHelper:
         dw.find_element_by_link_text("groups").click()
 
 
+    def fill_group_form(self, group):
+        dw = self.app.dw
+        dw.find_element_by_name("group_name").send_keys(group.name)
+        dw.find_element_by_name("group_header").send_keys(group.header)
+        dw.find_element_by_name("group_footer").send_keys(group.footer)
+
+
     def create(self, group):
         dw = self.app.dw
         self.open_groups_page()
         # init group creation
         dw.find_element_by_name("new").click()
-        # fill out group form
-        dw.find_element_by_name("group_name").send_keys(group.name)
-        dw.find_element_by_name("group_header").send_keys(group.header)
-        dw.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group form creation
         dw.find_element_by_name("submit").click()
         self.return_to_groups_page()
@@ -52,14 +56,10 @@ class GroupHelper:
         self.open_groups_page()
         # select first group
         dw.find_element_by_name("selected[]").click()
-        # submit edition
+        # submit editing
         dw.find_element_by_name("edit").click()
-        # clear edit group form
         self.clear_group_form()
-        # fill out edit group form
-        dw.find_element_by_name("group_name").send_keys(group.name)
-        dw.find_element_by_name("group_header").send_keys(group.header)
-        dw.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit edit group form
         dw.find_element_by_name("update").click()
         self.return_to_groups_page()
