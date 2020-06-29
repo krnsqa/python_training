9cec4cd4cf7bf4a6d2006b866ccd6366d41aff4a
Krn <katerina.nnnn@gmail.com>
2020-06-28 19:26:27 -0500

    Реализованы проверки в виде сравнения списков

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,18 +2,22 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    app.contact.modify_first_contact(Contact(firstname="new firstname"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
-
-
-def test_modify_contact_lastname(app):
     old_contacts = app.contact.get_contact_list()
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="test"))
-    app.contact.modify_first_contact(Contact(lastname="new lastname"))
+    contact = Contact(firstname="new firstname")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+#def test_modify_contact_lastname(app):
+#    if app.contact.count() == 0:
+#        app.contact.create(Contact(firstname="test"))
+#    old_contacts = app.contact.get_contact_list()
+#    app.contact.modify_first_contact(Contact(lastname="new lastname"))
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts) == len(new_contacts)
