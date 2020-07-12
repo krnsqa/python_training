6df399d58ad5bb9e7dbe8e6b4a496a9a758c8d6d
Krn <katerina.nnnn@gmail.com>
2020-07-11 20:06:54 -0500

    Реализована загрузка информации о тестируемом приложении из конфигурационного файла.

--- test/test_phones.py
+++ test/test_phones.py
@@ -10,10 +10,10 @@ def test_phones_on_homepage(app):
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.homephone == (contact_from_edit_page.homephone)
-    assert contact_from_view_page.mobile == (contact_from_edit_page.mobile)
-    assert contact_from_view_page.workphone == (contact_from_edit_page.workphone)
-    assert contact_from_view_page.secondaryphone == (contact_from_edit_page.secondaryphone)
+    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
 
 
 def clear(s):
